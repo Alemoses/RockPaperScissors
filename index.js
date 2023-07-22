1743e8cf3cfd3284157256557ed8d13bdf3d0c71
Alex <alexnowrang@gmail.com>
2023-07-22 17:54:40 -0500

    Fix user input logic and add comments descibing purpose

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Logic for getting the computer's choice in the game at random//
+
 const getComputerChoice = () => {
   let computerChoice = Math.floor(Math.random() * 3);
     
@@ -10,18 +12,20 @@ const getComputerChoice = () => {
   };
 }
 
-let playRound = (playerSelection, computerSelection) => {
-  if (playerSelection === 'rock'&& getComputerChoice === 'paper') {
-    return "You lost this round!"
-  } else if (playerSelection === 'paper' && getComputerChoice === 'scissors') {
-    return "You lost this round!"
-  } else if (playerSelection === "scissors" && getComputerChoice === 'rock') {
-    return "You lost this round"
-  } else if (playerSelection === getComputerChoice) {
-    return "This round resulted in a tie!"
+//Logic for the user's input and the game round results//
+
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection === 'rock'&& getComputerChoice() === 'paper') {
+    return "You lost this round!";
+  } else if (playerSelection === 'paper' && getComputerChoice() === 'scissors') {
+    return "You lost this round!";
+  } else if (playerSelection === "scissors" && getComputerChoice() === 'rock') {
+    return "You lost this round";
+  } else if (playerSelection === getComputerChoice()) {
+    return "This round resulted in a tie!";
   } else {
-    return "You win this round"
-  };
+    return "You win this round";
+  }
 }
 
 const playerSelection = 'rock';
